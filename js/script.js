ed54f688f72d5eb8a3c5cfd9cbe1bc8d39603288
mgirma <mgirma173@gmail.com>
2019-11-20 22:50:21 +0000

    i updated the javascript

--- js/script.js
+++ js/script.js
@@ -1,4 +1,20 @@
 $( "document" ).ready(function() { //this helps your jQuery to work. Write all of your JS code in this function.
+$("#button").click(function(){
+let userInput=$("input").val();
+userInput=userInput.toLowercase();
+let vowel= ("a","e","i","o","u");
+let word=userInput.split(" ");
+let result= [];
+word.forEach(function(input) {
+    let vowelIndex= 0;
+    if(vowel.includes(input[0])){
+.charAt(1);
+
+.slice(1);
+
+var vowel= ("a","e","i","o","u");
+
+
 
 
 
